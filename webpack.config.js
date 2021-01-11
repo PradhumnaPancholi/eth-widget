1837ddaf9d878b71d6a040f832532d3b9dfb65a7
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2021-01-10 20:35:02 -0500

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
+
+const libraryName = 'idea-markets-plugin'; 
+const outputFile = `${libraryName}.min.js`;
+
+
+module.exports = {
+  entry: [
+    "webpack-dev-server/client?http://127.0.0.0:8080/",
+    "webpack/hot/only-dev-server",
+    './src/index.js'
+  ],
+  output: {
+    library: libraryName,
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    path: path.resolve(__dirname, 'dist'),
+    filename: outputFile,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 20000, // Convert images < 8kb to base64 strings
+            name: 'img/[hash]-[name].[ext]',
+          },
+        }],
+      },
+    ],
+  },
+  plugins: [
+    new uglifyJsPlugin(),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
